2246f557fd4afa4206b4dd4327b18910bf03665d
Chris Douglass <cedouglass@gmail.com>
2023-02-13 14:37:33 -0500

    sessions enabled, session list initialized, routing for homepage and new list with basic functionality

---
+++ todo.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get '/' do
+  redirect '/lists'
+end
+
+get '/lists' do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+get '/lists/new' do
+  erb :new_list, layout: :layout
+end
+
+post '/lists/new' do
+  session[:lists] << {name: params[:list_name], todos: []}
+  redirect '/lists'
+end
