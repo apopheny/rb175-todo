53148a6a4b32a63af316662ce51abb15a1f4148d
Chris Douglass <cedouglass@gmail.com>
2023-02-14 08:56:05 -0500

    added flash message for new list creation

--- todo.rb
+++ todo.rb
@@ -3,6 +3,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'pry'
 
 configure do
   enable :sessions
@@ -17,16 +18,26 @@ get '/' do
   redirect '/lists'
 end
 
+# View of all lists
 get '/lists' do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+# Renders the new list form
 get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
+# Hash of potential flash messages
+MESSAGES = {
+  new_list_success: 'The list has been created'
+}.freeze
+
+# Creates a new list
 post '/lists/new' do
-  session[:lists] << {name: params[:list_name], todos: []}
+  session[:lists] << { name: params[:list_name], todos: [] }
+  session[:message] = MESSAGES[:new_list_success]
+
   redirect '/lists'
 end
