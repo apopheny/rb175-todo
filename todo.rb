10fb4a2ae7042a820d8d3975197af06e275e522b
Chris Douglass <cedouglass@gmail.com>
2023-02-14 14:24:18 -0500

    list name validation and error messages

--- todo.rb
+++ todo.rb
@@ -31,13 +31,29 @@ end
 
 # Hash of potential flash messages
 MESSAGES = {
-  new_list_success: 'The list has been created'
+  new_list_success: 'The list has been created.',
+  list_length_error:  'List name must be between 1-100 characters.',
+  list_duplicate_name: 'List name already exists. Please choose a unique name.'
 }.freeze
+
+# Validates list name length and uniqueness
+def valid_list?(name)
+  return {valid: false, error: MESSAGES[:list_length_error]} unless (1..100).cover?(name.size)  
+  return {valid: false, error: MESSAGES[:list_duplicate_name]}\
+    if (session[:lists].any? { |list| list[:name] == name })
+  {valid: true}
+end
 
 # Creates a new list
 post '/lists/new' do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  session[:message] = MESSAGES[:new_list_success]
-
-  redirect '/lists'
+  list_name = params[:list_name].strip || ''
+  name_status = valid_list?(list_name)
+  if name_status[:valid]
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = MESSAGES[:new_list_success]
+    redirect '/lists'
+  else
+    session[:error] = name_status[:error]
+    erb :new_list, layout: :layout
+  end
 end
